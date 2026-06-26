0686cb384b6c7e451518a8916f772c7c3c88ec50
Justin178878 <justinpark@mds.ac.nz>
2026-06-26 16:02:48 +1200

    added

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const SUPABASE_KEY = "sb_publishable_HVeUO2IeF-ButWS-Hd8HIA_sXPcJ1Wp";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
+
+
+const selectBoxing = document.querySelector("#boxing")
+const closeButton = document.querySelector(".close-btn")
+
+selectBoxing.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    window.location.href = 'boxing.html';
+})
+
+closeButton.addEventListener('click', function(event){
+    event.preventDefault();
+
+    window.location.href = 'app.html';
+})
