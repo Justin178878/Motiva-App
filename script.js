291bc015d4f44f992f09e6b576392b8d13dfcd16
Justin178878 <justinpark@mds.ac.nz>
2026-06-26 15:24:12 +1200

    good

---
+++ script.js
@@ -0,0 +1,7 @@
+import {createClient} from "https://esm.sh/@supabase/supabase-js";
+
+const SUPABASE_URL = "https://isanuixzxksbkqxsimye.supabase.co";
+const SUPABASE_KEY = "sb_publishable_HVeUO2IeF-ButWS-Hd8HIA_sXPcJ1Wp";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
